ff5382898bba8a577fca4bdd6c0b87b85f1d2d84
repl.it user <replituser@example.com>
2021-03-04 20:31:50 +0000

    hi

---
+++ basics/conditionals/basic_if.py
@@ -0,0 +1,10 @@
+name = input("what is your name: ")
+age = int(input("what is your age :"))
+
+if (age>=18):
+  print("Hello" , name , "we are happy to serve you alcohol") 
+else :
+  print ("Hello" , name , "please come back later")
+
+
+
---
+++ basics/conditionals/short_if.py
@@ -0,0 +1,8 @@
+balance = 500
+withdraw = 500
+
+balance = balance - withdraw
+print("your final balance is",balance)
+
+if(balance<=0):
+  print("Warning! you are now in overdraft")
