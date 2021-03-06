f18dea514448e4dbfe929606a16d50156c564845
repl.it user <replituser@example.com>
2021-03-06 18:32:12 +0000

    sup

---
+++ basics/Lessons/week2/week2exercise.py
@@ -0,0 +1,46 @@
+# balance  = int(input("What is the balance: "))
+
+
+# if(balance >= 0):
+#   if(balance > 10):
+#    print("positive significant")
+#   else:
+#    print("possitive insignificalt")
+# else:
+#  if(balance < -10):
+#   print("negative significant")
+#  else:
+#   print("negative insignificalt")
+
+
+#   if (balance >0):
+#     print("positive")
+#   else:
+#     print("negative") 
+    
+#   if abs(balance >10):
+#     print("significant")
+#   else:
+#     print("insignificant") 
+
+# weather = input("What is the weather like (sunny/rainy): ")
+
+
+# if(weather == "sunny"):
+#   print("Its sunny")
+#   choice = input("what would you like to do (golf/football:")
+#   if(choice == "football"):
+#     print("Football forever!")
+#   else:
+#     print("Golf forever")
+# else:
+#   print("its raining")
+#   choice = input("what would you like to do (reading/studying:")
+#   if(choice == "reading"):
+#     print("reading forever!")
+#   else:
+#     print("studying forever")  
+
+for a in range(1,4):
+  for i in range(2,6):
+   print(i, end=" ")
