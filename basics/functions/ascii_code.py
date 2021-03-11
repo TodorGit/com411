88909ebfe1103823328e9cb61dcb48230e66dd4c
repl.it user <replituser@example.com>
2021-03-11 22:55:12 +0000

    added code to demonstrate use of built in functions

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,10 @@
+print("Program started")
+u_input = input("Please enter a letter: ")
+
+
+if (len(u_input)!=1):
+  print("Please enter a valid input")
+else:
+ print("The ASCII code for " + u_input + " is: ",ord(u_input))
+ print("Program ended")
+ print("the next character is",)
