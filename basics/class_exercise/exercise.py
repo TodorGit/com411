e5b8af3958c0868ef8a917dd801031198ec7c6e9
Replit user <replituser@example.com>
2021-03-31 20:28:02 +0000

    Homework

---
+++ basics/class_exercise/exercise.py
@@ -0,0 +1,52 @@
+# a = [3,8,-5,2,0]
+
+# b = len(a)
+
+# for i in a:
+#   print(i)
+#   print(b)
+
+# a = [3,8,-5,2,0]
+
+# print(len(a))
+
+
+
+
+# a = [3,8,-5]
+
+
+# for i in a:
+#   print(i)
+
+
+
+# for x in range(len(a)):
+#   print(a[x])
+
+# x = [5,5,5]
+# y = "hello"
+# q = [[1,2,3],[4,5,6],[7,8,9],[1,0,1]]
+
+# w = ""
+
+# for z in y:
+#   w += z
+# print(w)
+
+
+# T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+# for r in T:
+#     for c in r:
+#         print(c,end = " ")
+#     print()
+
+
+# T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+
+# T[2] = [11,9]
+# T[0][3] = 7
+# for r in T:
+#     for c in r:
+#         print(c,end = " ")
+#     print()
