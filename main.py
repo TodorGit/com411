73937b3bbb133dff3dafb3c0af707b0db267d757
Replit user <replituser@example.com>
2021-03-31 20:24:28 +0000

    Homework

--- main.py
+++ main.py
@@ -62,24 +62,93 @@
   
 
 
+# a = [3,8,-5,2,0]
+
+# b = len(a)
+
+# for i in a:
+#   print(i)
+#   print(b)
+
+# a = [3,8,-5,2,0]
+
+# print(len(a))
 
 
 
 
+# a = [3,8,-5]
+
+
+# for i in a:
+#   print(i)
 
 
 
+# for x in range(len(a)):
+#   print(a[x])
+
+# x = [5,5,5]
+# y = "hello"
+# q = [[1,2,3],[4,5,6],[7,8,9],[1,0,1]]
+
+# w = ""
+
+# for z in y:
+#   w += z
+# print(w)
 
 
+# T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+# for r in T:
+#     for c in r:
+#         print(c,end = " ")
+#     print()
 
 
+# T = [[11, 12, 5, 2], [15, 6,10], [10, 8, 12, 5], [12,15,8,6]]
+
+# T[2] = [11,9]
+# T[0][3] = 7
+# for r in T:
+#     for c in r:
+#         print(c,end = " ")
+#     print()
 
 
+a = [1,5,5,0,2]
+
+frog = 0 
+for i in a:
+  frog= frog + 1 
+print (frog)
+
+a = [1,5,5,0,2]
+frog = 1
+
+for i in a:
+	frog = frog * i
+
+print(frog)
 
 
+a = [1,5,5,0,2];     
+    
+min = a[0];    
+     
+for i in range(0, len(a)):    
+   if(a[i] < min):    
+       min = a[i];    
+     
+print("Smallest element present in given array: " + str(min)); 
 
 
-
-
-
-
+a= [1,5,5,0,2];     
+     
+max = a[0];    
+     
+for i in range(0, len(a)):    
+   if(a[i] > max):    
+       max = a[i];    
+           
+print("Largest element present in given array: " + str(max));   
